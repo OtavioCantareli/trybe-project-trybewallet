2a9b5333e8204965ad98821b418393989f3b8858
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-10 17:18:40 -0300

    Página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="email" data-testid="email-input" placeholder="Email" />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Password"
+        />
+        <button type="button" disabled>Entrar</button>
+      </div>
+    );
   }
 }
 
