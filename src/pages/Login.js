1699382a7c490b9a62834a05c9033c7c6927b89a
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-10 18:03:24 -0300

    Adicionada validação no login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,53 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailInput: '',
+      passInput: '',
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePassChange = this.handlePassChange.bind(this);
+  }
+
+  handleEmailChange = ({ target: { value } }) => {
+    this.setState({ emailInput: value });
+  };
+
+  handlePassChange = ({ target: { value } }) => {
+    this.setState({ passInput: value });
+  };
+
   render() {
+    const {
+      handlePassChange,
+      handleEmailChange,
+      state: { emailInput, passInput },
+    } = this;
+    const regex = /^\w+([.-]?\w+)+@\w+([.:]?\w+)+(\.[a-zA-Z0-9]{3})+$/;
+    const passLength = 5;
     return (
       <div>
-        <input type="email" data-testid="email-input" placeholder="Email" />
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+          onChange={ handleEmailChange }
+        />
         <input
           type="password"
           data-testid="password-input"
           placeholder="Password"
+          onChange={ handlePassChange }
         />
-        <button type="button" disabled>Entrar</button>
+        {regex.test(emailInput) && passInput.length > passLength ? (
+          <button type="button">Entrar</button>
+        ) : (
+          <button type="button" disabled>
+            Entrar
+          </button>
+        )}
       </div>
     );
   }
