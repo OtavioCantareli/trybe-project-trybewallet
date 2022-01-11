a16b1bc6a66c51711d7988b8f133511e887ad18b
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-11 15:52:57 -0300

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Expenses from './components/Expenses';
 import Header from './components/Header';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Expenses />
+      </>
     );
   }
 }
---
+++ src/pages/components/Expenses.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const API_ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
+
+export default class Expenses extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+    };
+  }
+
+  componentDidMount() {
+    this.saveResultToState();
+  }
+
+  async saveResultToState() {
+    const results = await fetch(API_ENDPOINT)
+      .then((response) => response.json())
+      .catch((error) => error);
+    this.setState({ results });
+  }
+
+  render() {
+    const { results } = this.state;
+    return (
+      <form>
+        <input type="number" data-testid="value-input" placeholder="Value" />
+        <input
+          type="text"
+          data-testid="description-input"
+          placeholder="Description"
+        />
+        <div>
+          <label htmlFor="currency-input">
+            Moeda:
+            <select data-testid="currency-input" id="currency-input">
+              {Object.keys(results)
+                .filter((items) => items !== 'USDT')
+                .map((result) => (
+                  <option data-testid={ `${result}` } key={ results[result].code }>
+                    {result}
+                  </option>
+                ))}
+            </select>
+          </label>
+        </div>
+        <div>
+          <select data-testid="method-input">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </div>
+        <div>
+          <select data-testid="tag-input">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </div>
+        <button type="button">Adicionar despesa</button>
+      </form>
+    );
+  }
+}
