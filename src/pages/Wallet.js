95eba2ef411007317986f51d857533e9c907b1fd
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-14 19:09:06 -0300

    Recomeçando: req 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import Expenses from './components/Expenses';
-import Header from './components/Header';
-import Table from './components/Table';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// import Expenses from './components/Expenses';
+// import Header from './components/Header';
+// import Table from './components/Table';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
-      <>
-        <Header />
-        <Expenses />
-        <Table />
-      </>
+      // <>
+      //   <Header />
+      //   <Expenses />
+      //   <Table />
+      // </>
+      <header>
+        <span data-testid="email-field">{ email }</span>
+        <p data-testid="total-field">0</p>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
     );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
