eda3f93f335a7b13ba36ceca377974fc9760db8f
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-17 13:00:30 -0300

    Nem lembro

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
 // Coloque aqui suas actions
+export const api = async () => {
+  const url = 'https://economia.awesomeapi.com.br/json/all';
+  const data = await fetch(url);
+  return data.json();
+};
 
 export const USER_EMAIL = 'USER_EMAIL';
 export const ADD_EXPENSE = 'ADD_EXPENSE';
@@ -12,8 +17,10 @@ export const getCurrencies = (currencies) => ({
 });
 
 export const fetchCurrencies = () => async (dispatch) => {
-  const result = await api('https://economia.awesomeapi.com.br/json/all');
-  const currencies = Object.keys(result).filter((currency) => currency !== 'USDT');
+  const result = await api();
+  const currencies = Object.keys(result).filter(
+    (currency) => currency !== 'USDT',
+  );
 
   dispatch(getCurrencies(currencies));
 };
