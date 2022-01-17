52f346bd2c8b95255661f9aa76b75ef37ef335c4
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-01-17 13:52:13 -0300

    SERÁ?!

--- src/pages/components/Expenses.jsx
+++ src/pages/components/Expenses.jsx
@@ -1,59 +1,115 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchCurrencies, dataExpenses, api } from '../../actions/index';
 
-const URL = 'https://economia.awesomeapi.com.br/json/all';
-
-export default class Expenses extends React.Component {
+class Expenses extends React.Component {
   constructor() {
     super();
     this.state = {
-      results: [],
+      id: 0,
+      value: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+      description: '',
     };
+    this.expenseSubmit = this.expenseSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    this.saveResultToState();
+    const { getCurrencies } = this.props;
+    getCurrencies();
   }
 
-  async saveResultToState() {
-    const results = await fetch(URL)
-      .then((response) => response.json())
-      .catch((error) => error);
-    this.setState({ results });
+  handleChange = (e) => {
+    const { id, value } = e.target;
+    this.setState({ [id]: value });
+  };
+
+  async expenseSubmit(event) {
+    event.preventDefault();
+    const { id, value, currency, method, tag, description } = this.state;
+    const { dataExpense } = this.props;
+    const exchangeRates = await api();
+    const expenseCard = {
+      id,
+      value,
+      currency,
+      method,
+      tag,
+      description,
+      exchangeRates,
+    };
+    this.setState((prev) => ({ id: prev.id + 1 }));
+    dataExpense(expenseCard);
+    this.setState({
+      value: '',
+      description: '',
+    });
   }
 
   render() {
-    const { results } = this.state;
+    const { value, currency, method, tag, description } = this.state;
+    const { currenciesArr } = this.props;
     return (
       <form>
-        <input type="number" data-testid="value-input" placeholder="Value" />
         <input
+          id="value"
+          name="value"
+          type="number"
+          value={ value }
+          data-testid="value-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          id="description"
+          name="description"
           type="text"
           data-testid="description-input"
-          placeholder="Description"
+          value={ description }
+          onChange={ this.handleChange }
         />
         <div>
-          <label htmlFor="currency-input">
+          <label htmlFor="currency">
             Moeda:
-            <select data-testid="currency-input" id="currency-input">
-              {Object.keys(results)
-                .filter((items) => items !== 'USDT')
-                .map((result) => (
-                  <option data-testid={ `${result}` } key={ results[result].code }>
-                    {result}
-                  </option>
-                ))}
+            <select
+              id="currency"
+              name="currency"
+              data-testid="currency-input"
+              value={ currency }
+              onChange={ this.handleChange }
+            >
+              {currenciesArr.map((c, i) => (
+                <option value={ c } key={ i } data-testid={ c }>
+                  {c}
+                </option>
+              ))}
             </select>
           </label>
         </div>
         <div>
-          <select data-testid="method-input">
+          <select
+            id="method"
+            name="method"
+            data-testid="method-input"
+            value={ method }
+            onChange={ this.handleChange }
+          >
             <option>Dinheiro</option>
             <option>Cartão de crédito</option>
             <option>Cartão de débito</option>
           </select>
         </div>
         <div>
-          <select data-testid="tag-input">
+          <select
+            data-testid="tag-input"
+            id="tag"
+            name="tag"
+            value={ tag }
+            onChange={ this.handleChange }
+          >
             <option>Alimentação</option>
             <option>Lazer</option>
             <option>Trabalho</option>
@@ -61,8 +117,30 @@ export default class Expenses extends React.Component {
             <option>Saúde</option>
           </select>
         </div>
-        <button type="button">Adicionar despesa</button>
+        <button
+          type="button"
+          onClick={ this.expenseSubmit }
+        >
+          Adicionar despesa
+        </button>
       </form>
     );
   }
 }
+
+Expenses.propTypes = {
+  getCurrencies: PropTypes.func.isRequired,
+  currenciesArr: PropTypes.arrayOf(PropTypes.any).isRequired,
+  dataExpense: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  currenciesArr: state.wallet.currencies,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getCurrencies: () => dispatch(fetchCurrencies()),
+  dataExpense: (expenses) => dispatch(dataExpenses(expenses)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Expenses);
--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -4,17 +4,18 @@ import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
-    const { email, total } = this.props;
+    const { email, expenses } = this.props;
+    const total = expenses
+      .reduce((acc, val) => {
+        acc
+          += val.value * val.exchangeRates[val.currency].ask;
+        return acc;
+      }, 0)
+      .toFixed(2);
     return (
       <header>
         <span data-testid="email-field">{email}</span>
-        <p data-testid="total-field">
-          {total
-            .reduce((acc, val) => val.value * val.exchangeRates[val.currency].ask,
-            // return acc;
-              0)
-            .toFixed(2)}
-        </p>
+        <p data-testid="total-field">{total}</p>
         <span data-testid="header-currency-field">BRL</span>
       </header>
     );
@@ -23,12 +24,12 @@ class Header extends React.Component {
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
-  total: PropTypes.arrayOf(PropTypes.object).isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
-  total: state.wallet.expenses,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Header);
